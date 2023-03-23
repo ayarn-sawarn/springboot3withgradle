7436895b4dd2a70614f465c640064422c717fbfa
ayarn-sawarn <ayarn.sawarn@oodles.io>
2023-03-23 22:50:33 +0530

    first gradle project

---
+++ src/main/java/com/ayarn/Workingspringboot3Application.java
@@ -0,0 +1,13 @@
+package com.ayarn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workingspringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workingspringboot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayarn/Workingspringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayarn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workingspringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
